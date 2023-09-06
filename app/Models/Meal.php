aeebaf39e5239cb6e098f74eda40c2bb6986866a
hirooooota <hirooooota@gmail.com>
2023-09-07 02:45:38 +0900

    課題1完了

---
+++ app/Models/Meal.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class Meal extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'category_id',
+        'body',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function getImageUrlAttribute()
+    {
+        return Storage::url('images/meals/' . $this->image);
+    }
+}
