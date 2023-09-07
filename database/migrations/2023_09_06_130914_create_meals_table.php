61d88b596cc0e7198fb8d4c970412f4428461eeb
hirooooota <hirooooota@gmail.com>
2023-09-07 17:54:11 +0900

    課題1修正

--- database/migrations/2023_09_06_130914_create_meals_table.php
+++ database/migrations/2023_09_06_130914_create_meals_table.php
@@ -14,10 +14,10 @@ return new class extends Migration
         Schema::create('meals', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->foreignId('category_id');
-            // ->constrained()
-            // ->cascadeOnUpdate()
-            // ->cascadeOnDelete();
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
             $table->text('body');
             $table->string('image');
             $table->foreignId('user_id')
